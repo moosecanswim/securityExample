3577fb1a5c37cfff80bd9155f2b77b0646955671
moosecanswim <kyle.gallagher.09@gmail.com>
2017-08-24 09:33:10 -0400

    added admin role

--- src/main/java/com/moosecanswim/security/SecurityConfiguration.java
+++ src/main/java/com/moosecanswim/security/SecurityConfiguration.java
@@ -13,17 +13,20 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception{
         http
                 .authorizeRequests()
+                    .antMatchers("/")
+                    .access("hasRole('ROLE_USER') or hasRole('ROLE_ADMIN')")
+                    .antMatchers("/admin").access("hasRole('ROLE_ADMIN')")
                 .anyRequest().authenticated()
                 .and()
-                .formLogin().loginPage("/login").permitAll()
-                .and()
-                .httpBasic();
+                .formLogin().loginPage("/login").permitAll();
     }
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception{
 
-        auth.inMemoryAuthentication().
-                withUser("user").password("password").roles("USER");
+        auth.inMemoryAuthentication()
+                .withUser("user").password("password").roles("USER")
+                .and()
+                .withUser("dave").password("begreat").roles("ADMIN");
 
         /***
          * to add additional accounts, remove the semicolon at the
